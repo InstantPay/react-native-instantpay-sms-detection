f8720b18c13d550f5e185fe76f4b02f82972b9c3
DJ <dhananjay.y@instantpay.in>
2024-07-21 10:32:44 +0530

    upds

---
+++ android/src/main/java/com/instantpaysmsdetection/InstantpaySmsDetectionPackage.kt
@@ -0,0 +1,17 @@
+package com.instantpaysmsdetection
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class InstantpaySmsDetectionPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(InstantpaySmsDetectionModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
